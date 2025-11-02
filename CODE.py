5445746f3b222c69404a0452faf0b8fae4c7c416
Rayyen09 <109933054+Rayyen09@users.noreply.github.com>
2025-11-02 23:04:23 +0700

    Adjust horizontal rule style in markdown

--- CODE.py
+++ CODE.py
@@ -335,7 +335,7 @@ elif st.session_state["menu"] == "Orders":
                         save_data(st.session_state["data_produksi"])
                         st.rerun()
             
-            st.markdown("<hr style='margin: 5px 0;'>", unsafe_allow_html=True)
+           st.markdown("<hr style='margin: 8px 0; opacity: 0.2;'>", unsafe_allow_html=True)
     else:
         st.info("📝 Belum ada order yang diinput.")
 
